58efdae4b9e3a9ad75cd81d92114fe77ac2b7d6c
MikkelAngeles <mikkelfreak@gmail.com>
2020-04-30 12:37:56 +0200

    Working spring application with embedded react.js. Tested .jar works and is portable

---
+++ src/main/java/mhel/itu/bachelor/shortestpathmap/ShortestPathMapApplication.java
@@ -0,0 +1,13 @@
+package mhel.itu.bachelor.shortestpathmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortestPathMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortestPathMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mhel/itu/bachelor/shortestpathmap/ShortestPathMapApplicationTests.java
@@ -0,0 +1,13 @@
+package mhel.itu.bachelor.shortestpathmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortestPathMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
